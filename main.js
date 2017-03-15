21bd92225696b9714564d965b4866d11a461376c
V97ug <s1220164take@gmail.com>
2017-03-15 20:33:34 +0900

    add js and style

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
-function showSlider(){
-  var sliderValue = document.getElementById("slider").value;
-  // alert("スライダーの値:" + sliderValue);
-  alert("トゲデマルからプレゼントをもらった！")
-}
-
-function clock(){
-  var now = new Date();
-
-  var hour = now.getHours();
-  var min = now.getMinutes();
-  var sec = now.getSeconds();
-
-  if (hour < 10) hour = "0" + hour;
-  if (min < 10) min = "0" + min
-  if (sec < 10) sec = "0" + sec
-
-  var timeElement = document.getElementById("time");
-  timeElement.innerHTML = hour + ":" + min + ":" + sec;
-
-  var year = now.getFullYear();
-  var month = now.getMonth() + 1;
-  var day = now.getDate();
-  var weekArray = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-  var week = weekArray[now.getDay()];
-  var dateElement = document.getElementById("date");
-  dateElement.innerHTML = year + "/" + month + "/" + day + " " + week;
-
-  setTimeout("clock()",1000);
+function showTogeMessage(){
+  var rand = Math.floor( Math.random() * 11000 + 10000);
+  alert("トゲデマルからプレゼントを" + rand + "個もらった！");
 }
